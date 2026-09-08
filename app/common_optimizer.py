c157c1959e19304cf56bbf848f6703998931bb5d
Maiya Yamashita <mai.yamashita@pathoslogos.co.jp>
2026-09-08 10:05:23 +0900

    fix: increase common optimizer timeout to 60 seconds

--- app/common_optimizer.py
+++ app/common_optimizer.py
@@ -566,7 +566,7 @@ def create_solver():
 
     # ★ 高速化設定
     # 1. タイムアウトを 3 秒に短縮（3秒以内でその時点の最良解を返す）
-    solver.parameters.max_time_in_seconds = 0.8
+    solver.parameters.max_time_in_seconds = 60
 
     # 2. 再現性を優先し、シングルスレッド・固定シードを基本とする
     #    （AddElement化によりモデルが大幅に軽量化されたため、
